cc84941a2f9882c05d941835d9f44cedfc5da717
AlinaCamomile <ar.camomile.design@gmail.com>
2019-06-04 16:57:26 +0200

    ASDFADFDFDZVX

--- assets/styles/Java_escape_the_web.js
+++ assets/styles/Java_escape_the_web.js
@@ -8,6 +8,32 @@ console.log('hi there');
 //     alert('Ouch! Stop poking me!');
 
 
-$('body').css('background-image', 'url(assets/images/two.png)');
-$('body').css('background-image', 'url(assets/images/c.png)');
-$('.winner').css('visibility', 'visible')
+// $('body').css('background-image', 'url(assets/images/two.png)');
+// $('body').css('background-image', 'url(assets/images/c.png)');
+// $('.winner').css('visibility', 'visible')
+
+
+let count_bottom = 0;
+let count_top = 0;
+$(window).scroll(function() {
+    
+    if ($(window).scrollTop() == 0) {
+        console.log;
+
+        count_top++;
+    }
+
+    if($(window).scrollTop() + $(window).height() > $(document).height() - 30) {
+        console.log("near bottom!");
+
+        if (count === 0) {
+            $('body').css('background-image', 'url(assets/images/two.png)');
+        } if (count == 1) {
+            $('body').css('background-image', 'url(assets/images/c.png)');
+            $('.winner').css('visibility', 'visible')
+        }
+
+        count++;
+        
+    }
+ });
